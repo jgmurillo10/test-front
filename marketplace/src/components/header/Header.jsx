00ca06cc54fc89264cf6f29b097f6bcbe66de20e
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2018-02-24 23:38:56 -0500

    sidebar filtering styling

--- marketplace/src/components/header/Header.jsx
+++ marketplace/src/components/header/Header.jsx
@@ -4,28 +4,6 @@ import PropTypes from "prop-types";
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 export default class HeaderMenu extends Component {
-      // for (var i = 0; i<categories.length; i++) {
-      //   let a = categories[i];
-      //   if(a.sublevels)
-      //   for(var j = 0; j < a.sublevels.length;j++){
-
-      //   }
-      // }
-
-
-       // <SubMenu title={<span><Icon type="setting" />Navigation Three - Submenu</span>}>
-       //        <MenuItemGroup title="Item 1">
-       //          <Menu.Item key="setting:1">Option 1</Menu.Item>
-       //          <Menu.Item key="setting:2">Option 2</Menu.Item>
-       //        </MenuItemGroup>
-       //        <MenuItemGroup title="Item 2">
-       //          <Menu.Item key="setting:3">Option 3</Menu.Item>
-       //          <Menu.Item key="setting:4">Option 4</Menu.Item>
-       //        </MenuItemGroup>
-       //      </SubMenu>
-       //      <Menu.Item key="alipay">
-       //        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
-       //      </Menu.Item>
   componentDidMount(){
 
   }
@@ -35,22 +13,16 @@ export default class HeaderMenu extends Component {
       current: e.key,
     });
   }
-  renderMenu = () => {
-    this.props.categories.map((d,i)=>{
-                  return (<Menu.Item key={d.id}>{d.name}</Menu.Item>)              
-    })
-  }
   onChange = (value) => {
     console.log(value);
   }
 
   render() {
     return (
-      <div className="header">
-        {/*<div className="header-logo">
-                  <Icon className="header-icon" type="shop" />
-                  <h3>EL</h3>
-                </div>*/}
+      <div className="header effect-7">
+        <div className="header-logo">
+          <Icon className="header-icon" type="shop" />
+        </div>
         
         <div className="header-menu">
           <Menu
@@ -72,9 +44,9 @@ export default class HeaderMenu extends Component {
            
           </Menu>
         </div>
-        {/*<div className="header-cart">
+        <div className="header-cart">
                   <Icon type="shopping-cart" />
-                </div>*/}
+        </div>
       </div>
          
     );
